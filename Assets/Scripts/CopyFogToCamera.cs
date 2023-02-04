801837bdb033e4298a9c8258d36072138c17ab5c
Andrew C. Wang <andyman@idumpling.com>
2023-02-03 23:58:17 -1000

    Sample scene with liko and hair

---
+++ Assets/Scripts/CopyFogToCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CopyFogToCamera : MonoBehaviour
+{
+	public Camera cam;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	private void OnEnable()
+	{
+		Refresh();
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		Refresh();
+	}
+
+	private void Refresh()
+	{
+		if (cam != null)
+		{
+			Color fogColor = RenderSettings.fogColor;
+			cam.backgroundColor = fogColor;
+		}
+	}
+}
