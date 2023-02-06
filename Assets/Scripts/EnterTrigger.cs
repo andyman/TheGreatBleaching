69faa63d5a6eee91b402b1a32daecbecefd15414
Andrew C. Wang <andyman@idumpling.com>
2023-02-05 14:38:31 -1000

    The finger

---
+++ Assets/Scripts/EnterTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class EnterTrigger : MonoBehaviour
+{
+	public UnityEvent enterEvent;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		enterEvent.Invoke();
+	}
+}
